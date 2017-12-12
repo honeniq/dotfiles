af51501ba7eb3716106edd548eb8e6cb97519014
honeniq <honeniq@gmail.com>
2017-12-12 22:53:02 +0900

    Fix fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,29 @@
+## Env
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x GOPATH $HOME/.go
 set -x GHQROOT (ghq root)
 
-set -x PATH $HOME/.go/bin $PATH
+if test -e $HOME/anaconda3/bin
+  set -x PATH $HOME/anaconda3/bin $PATH
+end
+
+## Fish Shell (with bobthefish)
+set -g theme_display_git yes
+set -g theme_display_git_untracked yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby yes
+set -g theme_display_user yes
+set -g theme_display_vi yes
+set -g theme_display_date no
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_color_scheme dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_project_dir_length 0
+set -g theme_newline_cursor yes
