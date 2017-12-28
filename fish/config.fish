47da412b49ea082500939ff999ec157fe89c3950
honeniq <honeniq@gmail.com>
2017-12-28 16:50:40 +0900

    peco関連のfishプラグインを追加

--- fish/config.fish
+++ fish/config.fish
@@ -31,3 +31,9 @@ set -g theme_color_scheme dark
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_project_dir_length 0
 set -g theme_newline_cursor yes
+
+## Peco
+function fish_user_key_bindings
+  bind \cr peco_select_history
+  bind \c] peco_select_ghq_repository
+end
