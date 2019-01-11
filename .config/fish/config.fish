a300bf30a436b0cd9bd231427fc934143eae8e90
honeniq <honeniq@gmail.com>
2019-01-11 17:23:06 +0900

    あいまい幅対策

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,7 @@ set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 set -g theme_color_scheme dracula
 set -g fish_prompt_pwd_dir_length 1
+set -g fish_ambiguous_width 2
 set -g theme_project_dir_length 0
 set -g theme_newline_cursor yes
 
