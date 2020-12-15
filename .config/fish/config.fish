300a81b780932a7f468af90e078ed68e41001120
honeniq <honeniq@gmail.com>
2020-12-15 22:38:22 +0900

    Fix config for M1 Mac

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,28 @@
 ## Env
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
-set -x GOPATH $HOME/.go
-set -x GHQROOT (ghq root)
 set -x PATH $HOME/.local/bin $PATH
 
 if test -e $HOME/.nodebrew/current/bin
   set -x PATH $HOME/.nodebrew/current/bin $PATH
 end
 
+if test -e $HOME/anaconda3/bin
+  set -x PATH $HOME/anaconda3/bin $PATH
+end
+
+## Go
+if test (uname -s) = "Darwin"
+  set -x GOPATH "/usr/local/go/bin/go"
+else
+  set -x GOPATH $HOME/.go
+end
+
 if test -e $HOME/.go/bin
   set -x PATH $HOME/.go/bin $PATH
 end
 
-if test -e $HOME/anaconda3/bin
-  set -x PATH $HOME/anaconda3/bin $PATH
-end
+set -x GHQROOT (ghq root)
 
 ## configs for shellder
 set -g theme_display_user yes
