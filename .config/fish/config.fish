087da02029208705bc65d259dc80720fa50be853
honeniq <honeniq@gmail.com>
2022-11-01 19:46:56 +0900

    Rust関係の設定を追加して整理

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,10 @@ if test -e $HOME/.nodebrew/current/bin
   set -x PATH $HOME/.nodebrew/current/bin $PATH
 end
 
+# ================
+# PATHs
+# ================
+## anaconda
 if test -e $HOME/anaconda3/bin
   set -x PATH $HOME/anaconda3/bin $PATH
 end
@@ -19,26 +23,32 @@ else
   set -x GOPATH $HOME/.go
   set -x PATH $HOME/.go/bin $PATH
 end
-
 set -x GHQROOT (ghq root)
 
-## configs for shellder
-set -g theme_display_user yes
-set -g theme_hostname never
+## Rust
+if test -e $HOME/.cargo/bin
+  set -x PATH $HOME/.cargo/bin $PATH
+end
 
+
+# ================
+# Commands
+# ================
 ## Peco
 function fish_user_key_bindings
   bind \cr peco_select_history
   bind \c] peco_select_ghq_repository
 end
 
+## AWS CLI
 ## if 'aws' command not exists, use docker container
 if test -e /usr/local/bin/aws
 else
   alias aws='docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli'
 end
 
-## if 'aws' command not exists, use docker container
+## GCP CLI(gcloud)
+## if 'gcloud' command not exists, use docker container
 if test -e ~/google-cloud-sdk/bin/gcloud
 else
   alias gcloud='docker run --rm  --volumes-from gcloud-config gcr.io/google.com/cloudsdktool/cloud-sdk:latest gcloud'
